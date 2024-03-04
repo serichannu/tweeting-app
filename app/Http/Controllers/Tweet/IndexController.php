d8718945255a8cc5e57c29d79e2d0afa06e809aa
serichannu <serina.chappy@icloud.com>
2024-03-04 11:57:29 +0900

    サービスコンテナ

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request) {
-        $tweets = Tweet::orderBy('created_at', 'desc')->get();
+    public function __invoke(Request $request, TweetService $tweetService) {
+        // $tweets = Tweet::orderBy('created_at', 'desc')->get();
         // dd($tweets);
+        // $tweetService = new TweetService();
+        $tweets = $tweetService->getTweets();
         return view('tweets.index')->with('tweets', $tweets);
 
     }
